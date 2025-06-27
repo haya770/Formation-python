a2b2f470b7bc73946aa8adc5c9ed6e47152bf8f6
Haya770 <haya_laloum@hotmail.fr>
2025-06-27 13:03:57 +0200

    saly=ut

---
+++ exercice2.py
@@ -0,0 +1,28 @@
+x = 4
+print (x)
+y = "Python"
+z = y[2]
+print(z)
+v  = 7.8
+print(type(v))
+z = int(v)
+print(type(z))
+#A = input("Ton nom ")
+#print(A)
+
+
+sample = "This is a sample sentence." 
+ABC = sample.split()[3]
+
+print(ABC)
+
+lis = ['apple', 'banana', 'cherry']
+print(lis)
+
+lis.append('date')
+print(lis)
+print(len(lis))
+#lis.remove(lis[1])
+#print(lis)
+lis.sort(reverse = True)
+print(lis)  
