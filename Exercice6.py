aad6e019578c9544cf3abe527b0c7b9bf913f88f
haya770 <haya_laloum@hotmail.fr>
2025-07-01 18:40:38 +0200

    juste fait 4 exos

--- Exercice6.py
+++ Exercice6.py
@@ -13,3 +13,19 @@ nums = [2,3,4,5]
 
 nums_carre = [i**2 for i in nums]
 print(nums_carre)
+
+#question 3
+
+nums = [1,2,3,4,5,6,7,8]  
+def filter_even(liste):
+    nums_even = [i  for i in nums if i%2==0]
+    print(nums_even)
+
+filter_even(nums)
+
+#question 4
+list1 = [1,2,3] 
+list2 = [4,5,6] 
+
+list3 = list1+list2
+print(list3)
