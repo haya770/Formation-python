c71640bcfe803564a0e86728ac7f070ee4ca7b3f
haya770 <haya_laloum@hotmail.fr>
2025-07-01 07:32:00 +0200

    juste fait 2 exos

---
+++ Exercice6.py
@@ -0,0 +1,15 @@
+#question 1
+
+strings = ['foo', 'bar', 'baz']  
+
+
+strings_prem = [ i[0] for i in strings]
+print(strings_prem)
+
+
+#question 2
+
+nums = [2,3,4,5]
+
+nums_carre = [i**2 for i in nums]
+print(nums_carre)
